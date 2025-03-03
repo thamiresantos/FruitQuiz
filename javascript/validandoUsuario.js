14cb4dec98ecf57c6d46410e2415a38a0cdf3793
Thamires Santos <106447459+thamiresantos@users.noreply.github.com>
2025-03-02 23:31:15 -0300

    Create validandoUsuario.js

---
+++ javascript/validandoUsuario.js
@@ -0,0 +1,14 @@
+// Função para saber o nome do usuario na primeira pagina
+function validarNome(){
+    let nome = document.getElementById('nome').value;
+   // let usuario = userNome.ariaValueMax.trim()
+    
+    let mensagem = document.getElementById("mensagem")
+    if (nome === "") {
+        //mensagem.textContent = "Insira como gostaria de ser chamado para continuar"
+     alert ('Para continuar, insira como gostaria de ser chamada(o)!!')
+   
+    } else {
+        window.location.href = 'pergunta01.html'
+    }
+}
